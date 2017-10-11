a27611eed72172f6a851a2bae8f3a06279e05fce
Israel Garcia <igarcia@itexico.net>
2017-10-10 22:05:20 -0500

    notifications test

--- djangoApp/damnificados/personas/views.py
+++ djangoApp/damnificados/personas/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import render
 from .models import Personas,PersonasHasLugares
 from .serializer import PersonasGetName,PersonasCreateSerializer,PersonasSerializer,PersonasHasLugaresSerializer
 
+import requests
+
 # Create your views here.
 
 class PersonasApi(APIView):
@@ -23,10 +25,20 @@ class PersonasApi(APIView):
         bato=PersonasSerializer(data=request.data)
         if bato.is_valid():
             bato.save()
+            self._sendPush('asdasdasd','')
             return Response(bato.data,status=status.HTTP_201_CREATED)
         else:
             return Response(bato.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def _sendPush(self,message, devicetoken):
+        baseUrl = 'https://fcm.googleapis.com/fcm/send'
+        header = {'Autorization':'key=AIzaSyB0k006LxEMxjpcL1bgz6CkAtEhX2UjQdY','Content-Type':'application/json'}
+        body = { 'notification': { 'title': 'Dev-F!!!!!!!', 'body': 'SADOT! UNO NUEVO! ', 'icon': 'firebase-logo.png', 'click_action': 'https://haydetodo-test.firebaseapp.com/'}, 'to':'eV6hrH5oXgg:APA91bHb2GX6GYbj-VmtjtdvKlrsYur_emeJL9B6G0MJ8VSqUkY2F4p6CK58tg7S28XRc_NEaRoBGJ9uid1xGu43XHVs7-ySEBnbppaX0vJ0_ZU6W4jT_MtuVlxbtA3VVWGNBarM2yaj'}
+
+        print('YEEEEEEEAAAAHHH')
+        resp = requests.post(baseUrl,data=body,headers=header)  
+        print('sdasdasd',resp)
+    
 class PersonaApi(APIView):
     def _getPersona(self, pk):
         try:
