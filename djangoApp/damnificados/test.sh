a9211e71beb9e9a6be054c6b7823cd9e4fc66c85
Israel Garcia <igarcia@itexico.net>
2017-10-17 21:26:43 -0500

    circleCI integration

---
+++ djangoApp/damnificados/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python manage.py makemigrations
+python manage.py migrate personas zero
+python manage.py migrate lugares zero
+python manage.py migrate articulo zero
+python manage.py migrate user zero
+python manage.py migrate test
