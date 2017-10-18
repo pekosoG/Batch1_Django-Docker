878de16b7957cbaca36a876ded2717a3a4b02afc
Israel Garcia <igarcia@itexico.net>
2017-10-17 21:42:00 -0500

    manage test error fix

--- djangoApp/damnificados/test.sh
+++ djangoApp/damnificados/test.sh
@@ -3,4 +3,4 @@ python manage.py migrate personas zero
 python manage.py migrate lugares zero
 python manage.py migrate articulo zero
 python manage.py migrate user zero
-python manage.py migrate test
+python manage.py test
